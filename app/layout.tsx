b6825aa9fd0bc6672f39a419b266d4f6dc148c95
Beterraba <ls6182315@gmail.com>
2023-09-29 18:24:09 -0300

    start commit

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from 'next'
+import { ReactNode } from 'react'
+
+export const meta: Metadata = {
+  title: 'Clone Tabnews',
+  description: 'A clone of Tabnews',
+}
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div>
+      <h1>Gato Toyota</h1>
+    </div>
+  )
+}
+
+export default page
