2478c5e47e5ae085339e8ea47e891ff6645ce710
Beterraba <ls6182315@gmail.com>
2025-02-13 14:14:47 -0300

    finish `/api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,7 +1,16 @@
 import test from "node:test";
-import assert from "node:assert";
+import assert, { equal } from "node:assert";
 
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
-  assert.equal(response.status, 200);
+  equal(response.status, 200);
+
+  const { updated_at, dependecies } = await response.json();
+
+  const parsedUpdatedAt = new Date(updated_at).toISOString();
+  equal(updated_at, parsedUpdatedAt);
+
+  equal(dependecies.database.version, "17.2");
+  equal(dependecies.database.max_connections, 100);
+  equal(dependecies.database.opened_connections, 1);
 });
