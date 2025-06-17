b17d2fa31d4a6d25c84705f32dec3d5573499f44
Beterraba <ls6182315@gmail.com>
2025-06-17 09:14:37 -0300

    add `/api/v1/migrations` endpoint

--- infra/database.js
+++ infra/database.js
@@ -1,18 +1,11 @@
-import { Client } from "pg";
+import {Client} from "pg";
 
 async function query(queryObj) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
-  });
+  let client
+
   try {
-    await client.connect();
-    const result = await client.query(queryObj);
-    return result;
+    client = await getNewClient();
+    return await client.query(queryObj);
   } catch (error) {
     console.error(error);
     throw error;
@@ -21,6 +14,35 @@ async function query(queryObj) {
   }
 }
 
-export default {
-  query: query,
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
+  });
+
+  await client.connect();
+  return client
+}
+
+const database = {
+  query,
+  getNewClient
 };
+
+
+
+export default database;
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "production";
+}
---
+++ infra/migrations/1746646077593_first-migration-test.js
@@ -0,0 +1,18 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export let shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export async function up(pgm) {}
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export async function down(pgm) {}
