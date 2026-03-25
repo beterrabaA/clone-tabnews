23f420cb4395657e55255e0dd16f0e855ae852dd
Beterraba <ls6182315@gmail.com>
2026-03-24 21:02:00 -0300

    fix: correct typo in dependencies object and add integration test for status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -22,7 +22,7 @@ async function status(request, response) {
 
   return response.status(200).json({
     updated_at: updatedAt,
-    dependecies: {
+    dependencies: {
       database: {
         version: databaseVersionValue,
         max_connections: parseInt(databaseMaxConnectionsValue),
