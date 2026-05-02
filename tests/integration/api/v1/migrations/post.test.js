dfa4474ffccc8623ef68d208cd17b22c8b501f00
Beterraba <ls6182315@gmail.com>
2026-05-02 00:22:15 -0300

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -7,30 +7,43 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public;");
 });
 
-test("POST to /api/v1/migrations should return 200", async () => {
-  const response1 = await fetch(`${webserver.getOrigin}/api/v1/migrations`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({}),
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("When there are pending migrations", () => {
+      test("For the first time, should return 201", async () => {
+        const response = await fetch(
+          `${webserver.getOrigin}/api/v1/migrations`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({}),
+          },
+        );
+        expect(response.status).toBe(201);
+
+        const response1Body = await response.json();
+        expect(Array.isArray(response1Body)).toBe(true);
+        expect(response1Body.length).toBeGreaterThan(0);
+      });
+      test("For the second time, should return 200", async () => {
+        const response = await fetch(
+          `${webserver.getOrigin}/api/v1/migrations`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({}),
+          },
+        );
+        expect(response.status).toBe(200);
+
+        const response2Body = await response.json();
+        expect(Array.isArray(response2Body)).toBe(true);
+        expect(response2Body.length).toBe(0);
+      });
+    });
   });
-  expect(response1.status).toBe(201);
-
-  const response1Body = await response1.json();
-  expect(Array.isArray(response1Body)).toBe(true);
-  expect(response1Body.length).toBeGreaterThan(0);
-
-  const response2 = await fetch(`${webserver.getOrigin}/api/v1/migrations`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({}),
-  });
-  expect(response2.status).toBe(200);
-
-  const response2Body = await response2.json();
-  expect(Array.isArray(response2Body)).toBe(true);
-  expect(response2Body.length).toBe(0);
 });
