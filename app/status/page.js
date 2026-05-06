8648e4f456eaeeca1b32630ac2c25b2b021fed30
Beterraba <ls6182315@gmail.com>
2026-05-06 18:31:45 -0300

    fix: fix

--- app/status/page.js
+++ app/status/page.js
@@ -1,9 +1,9 @@
 import { DatabaseStatus } from "@/components/DatabaseStatus";
 import { UpdatedAt } from "@/components/UpdatedAt";
-console.log(process.env.NEXT_PUBLIC_SITE_URL);
+import webserver from "@/infra/webserver";
 
 export default async function StatusPage() {
-  const response = await fetch(`${process.env.VERCEL_URL}/api/v1/status`);
+  const response = await fetch(`${webserver.getOrigin}/api/v1/status`);
   const data = await response.json();
   return (
     <div>
