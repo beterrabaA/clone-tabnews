2e48624a333d31350178391a5ceaa589acaa2055
Beterraba <ls6182315@gmail.com>
2026-05-14 20:18:52 -0300

    feat: add tests for HTTP methods on sessions endpoint to ensure 405 response for anonymous users

---
+++ tests/integration/api/v1/sessions/get.test.js
@@ -0,0 +1,28 @@
+import webserver from "@/infra/webserver";
+import { waitForAllServices } from "@/tests/orchestrator";
+
+beforeAll(async () => {
+  await waitForAllServices();
+});
+
+describe("GET /api/v1/sessions", () => {
+  describe("Anonymous user", () => {
+    test("Attempting to get from sessions endpoint, should return 405", async () => {
+      const responseBody = await fetch(
+        `${webserver.getOrigin}/api/v1/sessions`,
+      );
+
+      const responseJson = await responseBody.json();
+
+      expect(responseBody.status).toBe(405);
+
+      expect(responseJson).toEqual({
+        name: "MethodNotAllowedError",
+        message: "Método não permitido para este endpoint.",
+        status_code: 405,
+        action:
+          "Verifique se o método HTTP utilizado é permitido para este endpoint e corrija a requisição.",
+      });
+    });
+  });
+});
