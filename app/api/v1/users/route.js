a2f4721c05afad77d2a2f998bbb379121f94a88d
Beterraba <ls6182315@gmail.com>
2026-08-08 20:33:19 -0300

    feat: implement `/api/v1/user` endpoint with session renewal

--- app/api/v1/users/route.js
+++ app/api/v1/users/route.js
@@ -1,9 +1,39 @@
 import { NextResponse } from "next/server";
 import user from "@/models/user";
 import { custom405, custom500 } from "@/utils/response";
+import { cookies } from "next/headers";
+import session from "@/models/session";
+import webserver from "@/infra/webserver";
 
 export async function GET() {
-  return custom405();
+  const cookieStore = await cookies();
+  try {
+    const hasSession = cookieStore.has("session_id");
+    if (!hasSession) return custom405();
+
+    const sessionToken = cookieStore.get("session_id").value;
+    const sessionObject = await session.findOneValidByToken(sessionToken);
+
+    await session.renew(sessionObject.id);
+    const currUser = await user.findOneById(sessionObject.userId);
+
+    const response = NextResponse.json(currUser, { status: 200 });
+    response.cookies.set({
+      name: "session_id",
+      value: sessionToken,
+      path: "/",
+      httpOnly: true,
+      domain: webserver.getOrigin,
+      maxAge: 60 * 60 * 24 * 30, // 30 days
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      expires: sessionObject.expiresAt,
+    });
+
+    return response;
+  } catch (error) {
+    return custom500(error);
+  }
 }
 
 export async function POST(request) {
